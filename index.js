9b2193e630302837e94d25003df45b0b5c0c674f
Javier Rodriguez <javi.rodriguez.martinz@gmail.com>
2022-11-15 11:32:41 -0600

    added sider code for js

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+const sidebar = document.querySelector(`#side-bar`)
+const sadSpanish = document.querySelector(`#sad-spanish`) 
+const wrapper = document.querySelectorAll(`button`)
+// fetch data
+function fetchSpotify(playId){
+    fetch(`https://api.spotify.com/v1/playlists/${playId}`, {
+        method: `get`,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization':  `Bearer ` + token
+        }
+    })
+        .then(resp => resp.json())
+        .then(result => {
+            console.log(result.id)
+            sad(result.id)
+        })
+}
+//set id src to frame
+function setSideBar(id){
+       sidebar.src = `https://open.spotify.com/embed/playlist/${id}?si=zLUn-H4DTl2SxyKlpLo5gA&amp&size=detail&amp;theme=light`
+}
 
-
-
+function sad(result){
+    console.log(result)
+    wrapper.forEach(btn => {
+        btn.addEventListener(`click`, (e) => {
+            console.log(e.target.id)
+             var id = e.target.id
+             setSideBar(id)
+        })
+    })
+}
+ 
 
 
 
@@ -9,5 +41,6 @@
 
 
 document.addEventListener(`DOMContentLoaded`, (e) => {
-
+   sad()
+  
 })
